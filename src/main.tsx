8d571d599f7607b4ee87d609da6979458825d9c9
LASER-Yi <liangyi0007@gmail.com>
2022-04-03 12:25:01 +0800

    Fix style issues

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import ErrorBoundary from "./components/ErrorBoundary";
 
+// @eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const rootElement = document.getElementById("root")!;
 
 ReactDOM.createRoot(rootElement).render(
